be14583596753b6c86ef39d3903309434e0fb895
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2024-12-19 14:18:12 -0500

    Update the camera firmware

--- TurretSRC/RaspberryCode/Camera/picamera.py
+++ TurretSRC/RaspberryCode/Camera/picamera.py
@@ -1,6 +1,7 @@
 from picamera2 import Picamera2
 from IO.camera import Camera
 import cv2
+import numpy as np
 
 class PiCamera(Camera):
     def __init__(self, resolution=(640,480), cap_fps=30) -> None:
@@ -9,9 +10,17 @@ class PiCamera(Camera):
         config = self.camera.create_video_configuration(main={"format": 'YUV420',"size": (resolution[0], resolution[1])})
         self.camera.configure(config)
         self.camera.start()
+        self.raw_buffer:memoryview = None
     
 
-    def get_frame(self) -> cv2.typing.MatLike:
-        image = self.camera.capture_array()
-        return image
+    def get_frame(self) -> None:
+        self.raw_buffer = self.camera.capture_buffer()
+
+    def decode_frame(self) -> tuple[bool, cv2.typing.MatLike]:
+        if self.raw_buffer is None:
+            return False, np.array([])
+
+        raw_data = np.frombuffer(self.raw_buffer,dtype=np.uint8)
+        return True, raw_data.reshape((self.RESOLUTION[1] * 3 // 2, self.RESOLUTION[0]))
+
     
