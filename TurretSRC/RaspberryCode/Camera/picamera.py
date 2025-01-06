997cdd4156bba29a9ee76b5b628b4ae8c578acc2
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2025-01-05 22:06:56 -0500

    Change to use different sensor

--- TurretSRC/RaspberryCode/Camera/picamera.py
+++ TurretSRC/RaspberryCode/Camera/picamera.py
@@ -3,25 +3,27 @@ from IO.camera import Camera
 import cv2
 import numpy as np
 
+
 class PiCamera(Camera):
-    def __init__(self,camera_num,resolution=(640,480), cap_fps=30) -> None:
+    def __init__(self, camera_num, resolution=(640, 480), cap_fps=30) -> None:
         super().__init__(resolution, cap_fps)
         self.camera = Picamera2(camera_num)
         # Note here that RGB888 is actually BGR with 8 bits for each color (as defined in the libcamera/picamera2 docs)
-        config = self.camera.create_video_configuration(main={"format": 'RGB888',"size": (resolution[0], resolution[1])})
+        # We will hardcode the sensor size here because we are using picamera module 2.
+        config = self.camera.create_video_configuration(sensor={'output_size': (1640, 1232), 'bit_depth': 10},
+                                                        main={"format": 'RGB888',
+                                                              "size": (resolution[0], resolution[1])})
         self.camera.configure(config)
         self.camera.start()
-        self.raw_buffer:memoryview = None
-    
+        self.raw_frame: np.ndarray = None
 
     def get_frame(self) -> None:
-        self.raw_buffer = self.camera.capture_buffer()
+        self.raw_frame = self.camera.capture_array()
 
     def decode_frame(self) -> tuple[bool, cv2.typing.MatLike]:
-        if self.raw_buffer is None:
+        if self.raw_frame is None:
             return False, np.array([])
 
-        return True, np.reshape(np.frombuffer(self.raw_buffer,dtype=np.uint8),
-                                (self.RESOLUTION[1],self.RESOLUTION[0],3))
+        return True, cv2.resize(self.raw_frame, self.RESOLUTION, interpolation=cv2.INTER_AREA)
 
     
